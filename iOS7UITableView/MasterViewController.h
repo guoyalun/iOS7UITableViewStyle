7b68e0a61a13fddf53a92b56157f0009843a6ca5
zhoon <zhoonchen@gmail.com>
2014-01-06 20:59:26 +0800

    Initial Commit

---
+++ iOS7UITableView/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  iOS7UITableView
+//
+//  Created by zhoonchen on 14-1-6.
+//  Copyright (c) 2014年 zhoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
